6d950509a935d429975a267fcd929aa20d4428d4
Ah-Lun Tang <tang@ahlun.be>
2014-05-11 21:54:46 +0200

    fix following list

--- app/models/User.php
+++ app/models/User.php
@@ -98,7 +98,7 @@ class User extends Eloquent {
 			array_push($ids, $f->following_id);
 		}
 
-		$users = count($ids) > 0 ? User::whereIn('id', $ids)->get() : User::all();
+		$users = count($ids) > 0 ? User::whereIn('id', $ids)->get() : null;
 		return $users;
 	}
 
@@ -110,7 +110,7 @@ class User extends Eloquent {
 		foreach ($following as $f) {
 			array_push($ids, $f->following_id);
 		}
-		$users = count($ids) > 0 ? User::whereNotIn('id', $ids)->get() : null;
+		$users = count($ids) > 0 ? User::whereNotIn('id', $ids)->get() : User::all();
 		return $users;
 	}
 
